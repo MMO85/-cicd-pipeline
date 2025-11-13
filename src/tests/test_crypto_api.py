a1e1b6ed2cb9a1d2c747b670bb3df17e078dceee
Maryam Moghadasi <moghadasi.64@gmail.com>
2025-11-13 19:43:40 +0100

    test

--- src/tests/test_crypto_api.py
+++ src/tests/test_crypto_api.py
@@ -1,24 +1,61 @@
 from webapp.crypto_api import transform, get_response
 import types
 
+
 def test_transform():
-    sample = {
-        "data": {"BTC": [{"quote": {"USD": {"price": 12345.67}}}]}  
+    test_fake_data = {
+   
+        "data": {
+            "BTC": [
+                    {
+                        "id": 1,
+                        "name": "Bitcoin",
+                        "symbol": "BTC",
+                            "quote": {
+                            "USD": {
+                                    "price": 103929.32609335119,
+                                
+                                }
+                        }
+                    },
+            ]
+        }
     }
-    assert transform(sample) == 12345.67
+    result = transform(test_fake_data)
+ 
 
 def test_response(monkeypatch):
     class DummyResp:
         status_code = 200
-        url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC"
+        url = "https://example.com"
         def raise_for_status(self): pass
         def json(self):
-            return {"data": {"BTC": [{"quote": {"USD": {"price": 12345.67}}}]}}
-    def fake_get(url, headers, params, timeout):
+            return {
+                "data": {
+                    "BTC": [
+                        {
+                            "symbol": "BTC",
+                            "quote": {"USD": {"price": 12345.67}}
+                        }
+                    ]
+                }
+            }
+
+    def fake_get(url, headers=None, params=None, timeout=10):
         return DummyResp()
 
     monkeypatch.setattr("webapp.crypto_api.requests.get", fake_get)
 
     resp = get_response()
     assert resp.status_code == 200
-    assert transform(resp.json()) == 12345.67
+
+    result = transform(resp.json())
+    assert result["symbol"] == "BTC"
+    assert result["price"] == 12345.67
+
+    
+
+    
+    
+
+
